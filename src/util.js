7e4a35f730d3c1ec474f7240d56001a44cc5c1b6
Davis Ford <ford_davis@bah.com>
2018-02-17 00:11:21 -0500

    moved things out to a Util file. started long_load, but temporary checksum is working

---
+++ src/util.js
@@ -0,0 +1,87 @@
+const Util = {}
+const exclamations = ['Dope!', 'Sick!', 'Wow!', 'Really nice work -',
+  'Incredible!!!', "That's tight -", 'Wicked sick!', 'My friend, you have astonished me.',
+  'Could it be? YES!', "Honestly, I'm impressed -", "Superlative!", "Can you believe it?",
+  'Outstanding!', '[Generic Positive Expression]!', 'I can tell you are smart -', 'Nerd - jk -'];
+Util.getExpression = () => exclamations[Math.floor(Math.random() * exclamations.length)];
+
+/**
+ * Removes unnecessary characters from a string
+ * @param txt
+ * @returns {string}
+ */
+Util.stripText = (txt) => {
+  return txt
+      .replace(/[‘’\u2018\u2019\u201A]/gm, "'") // smart single quotes and apostrophe
+      .replace(/[“”\u201C\u201D\u201E]/gm, '"') // smart double quotes
+      .replace(/\u2026/gm, "...")               // ellipsis
+      .replace(/\u02C6/gm, "^")                 // circumflex
+      .replace(/\u2039/gm, "<")                 // open angle bracket
+      .replace(/\u203A/gm, ">")                 // close angle bracket
+      .replace(/[\u02DC\u00A0]/gm, " ")         // spaces
+      .replace(/\d+:\d+/gm, " ")                // bible verses, lol, i.e. 31:12
+      .replace(/[\u2013\u2014,:;\-\+]/gm, " ")  // unnecessary punctuation
+      .replace(/([!\?]|\.{2,})/gm, ".")         // replace !?... with . to save time parsing later
+      .replace(/(\r\n|\n|\r)/gm, " ")           // strip newlines
+      .replace(/\s{2,}/gm, " ")                 // anything above 1 space is unnecessary
+};
+
+
+Util.getText = (txt) => {
+  console.log('Text length before stripping: ' + txt.length);
+  txt = Util.stripText(txt).trim().toLowerCase();
+  console.log('Text length after stripping: ' + txt.length);
+  return txt;
+};
+
+Util.getSimpleParse = (txt) => {
+  const sentences = txt.match(/[^\.!\?]+[\.!\?]+/g);
+  return sentences.map(x => x.split(' ').length)
+};
+
+Util.getSimplePathStr = (array_of_ints) => {
+  /* Turn left 90 degrees each time */
+  const behavior_ref = ['h -', 'v ', 'h ', 'v -'];
+  let count = 0;
+  return array_of_ints.reduce((a, i) => {
+    const move = `${behavior_ref[count]}${i}`;
+    count = count === 3 ? 0 : count + 1;
+    return `${a} ${move}`;
+  }, 'M50 20j')
+};
+
+
+Util.getSplitParse = (input_text, split_dict, primary_color) => {
+  const sentences = input_text.match(/[^\.!\?]+[\.!\?]+/g);
+  const default_color = primary_color || "#14B6D4";
+
+  return sentences.map(words => {
+    let segment_color = default_color
+    split_dict['words'].forEach(word => {
+      if (words.includes(word.toLowerCase())) {
+        segment_color = split_dict['color'] || "#F22F00"
+      }
+    });
+    return {'color': segment_color, 'length': words.split(' ').length}
+  })
+};
+
+/**
+ *
+ * @param txt
+ * @returns {string|null}
+ */
+Util.toHex = (txt) => {
+  txt = txt ? txt.trim() : '';
+  if (txt.includes('#') && Util.isHexCode(txt)) return txt;
+  return Util.isHexCode(`#${txt}`) ? `#${txt}` : null;
+};
+
+
+Util.getSplitText = (txt) => txt ? txt.split(',').map(x => x.trim().toLowerCase()) : null;
+Util.getNodeColors = (txt) => txt ? txt.split(',').map(x => Util.toHex(x)) : null;
+Util.isHexCode = (code) => /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(code);
+Util.checksum = (txt) => txt.split('').reduce((a, s, i) => a + (txt.charCodeAt(i) * (i + 1)), 0x12345678).toString();
+Util.reduceObj = (obj) => JSON.stringify(obj).replace(/\s/g, '');
+
+module.exports = Util
--- src/word-art.js
+++ src/word-art.js
@@ -1,15 +1,13 @@
+const Util = require('./util');
+// const LongLoad = require('./long_load')
+
 $(document).ready(() => {
   const version = 1;
   const endpoint = `https://gy2aa8c57c.execute-api.us-east-1.amazonaws.com/dev/`;
   const form = $('#word_art_form');
   const results = $('#display_results');
   const loading = $('#display_loading');
-
-  const exclamations = ['Dope!', 'Sick!', 'Wow!', 'Really nice work -',
-    'Incredible!!!', "That's tight -", 'Wicked sick!', 'My friend, you have astonished me.',
-    'Could it be? YES!', "Honestly, I'm impressed -", "Superlative!", "Can you believe it?",
-    'Outstanding!', '[Generic Positive Expression]!', 'I can tell you are smart -']
-  const getExpression = () => exclamations[Math.floor(Math.random() * exclamations.length)];
+  const long_load = $('#display_long_load');
 
   const start_load = () => {
     form.hide();
@@ -18,14 +16,20 @@ $(document).ready(() => {
   };
 
   const end_load = () => {
-    loading.html('').hide();
+    loading.html(null).hide();
+    long_load.html(null).hide()
     results.show();
     form.show();
   };
 
+  const long_load_screen = (url) => {
+    return `<p class="text-danger">Your SVG file is taking a while to generate.<br/>
+    It will eventually be uploaded to this url: <a href="${url}"><strong>${url}</strong></a> 
+    <br/>Try clicking this link in a minute or two :)</p>`;
+  };
   const load = () => `<p class="lead text-info text-center">Now generating your SVG file...</p>`;
   const success = (url) => {
-    return `<p class="lead text-success text-center">${getExpression()} Your SVG file has been generated</p>
+    return `<p class="lead text-success text-center">${Util.getExpression()} Your SVG file has been generated</p>
     <a href="${url}" target="_blank" role="button" class="btn btn-md btn-success">Download SVG</a>`
   };
   const error = (err) => {
@@ -34,83 +38,6 @@ $(document).ready(() => {
     <br/>Huge books (like the Bible) can sometimes overload the server.</p>`;
   };
 
-  /**
-   * Removes unnecessary characters from a string
-   * @param txt
-   * @returns {string}
-   */
-  const stripText = (txt) => {
-    return txt
-        .replace(/[‘’\u2018\u2019\u201A]/gm, "'") // smart single quotes and apostrophe
-        .replace(/[“”\u201C\u201D\u201E]/gm, '"') // smart double quotes
-        .replace(/\u2026/gm, "...")               // ellipsis
-        .replace(/\u02C6/gm, "^")                 // circumflex
-        .replace(/\u2039/gm, "<")                 // open angle bracket
-        .replace(/\u203A/gm, ">")                 // close angle bracket
-        .replace(/[\u02DC\u00A0]/gm, " ")         // spaces
-        .replace(/\d+:\d+/gm, " ")                // bible verses, lol, i.e. 31:12
-        .replace(/[\u2013\u2014,:;\-\+]/gm, " ")  // unnecessary punctuation
-        .replace(/([!\?]|\.{2,})/gm, ".")         // replace !?... with . to save time parsing later
-        .replace(/(\r\n|\n|\r)/gm, " ")           // strip newlines
-        .replace(/\s{2,}/gm, " ")                 // anything above 1 space is unnecessary
-  };
-
-
-  const getText = (txt) => {
-    console.log('Text length before stripping: ' + txt.length);
-    txt = stripText(txt).trim().toLowerCase();
-    console.log('Text length after stripping: ' + txt.length);
-    return txt;
-  };
-
-  const getSimpleParse = (txt) => {
-    const sentences = txt.match(/[^\.!\?]+[\.!\?]+/g);
-    return sentences.map(x => x.split(' ').length)
-  };
-
-  const getSimplePathStr = (array_of_ints) => {
-    /* Turn left 90 degrees each time */
-    const behavior_ref = ['h -', 'v ', 'h ', 'v -'];
-    let count = 0;
-    return array_of_ints.reduce((a, i) => {
-      const move = `${behavior_ref[count]}${i}`;
-      count = count === 3 ? 0 : count + 1;
-      return `${a} ${move}`;
-    }, 'M50 20j')
-  };
-
-
-  const getSplitParse = (input_text, split_dict, primary_color) => {
-    const sentences = input_text.match(/[^\.!\?]+[\.!\?]+/g);
-    const default_color = primary_color || "#14B6D4";
-
-    return sentences.map(words => {
-      let segment_color = default_color
-      split_dict['words'].forEach(word => {
-        if (words.includes(word.toLowerCase())) {
-          segment_color = split_dict['color'] || "#F22F00"
-        }
-      });
-      return {'color': segment_color, 'length': words.split(' ').length}
-    })
-  };
-
-  /**
-   *
-   * @param txt
-   * @returns {string|null}
-   */
-  const toHex = (txt) => {
-    txt = txt ? txt.trim() : '';
-    if (txt.includes('#') && isHexCode(txt)) return txt;
-    return isHexCode(`#${txt}`) ? `#${txt}` : null;
-  };
-
-
-  const getSplitText = (txt) => txt ? txt.split(',').map(x => x.trim().toLowerCase()) : null;
-  const getNodeColors = (txt) => txt ? txt.split(',').map(x => toHex(x)) : null;
-  const isHexCode = (code) => /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(code);
-  const checksum = (txt) => txt.split('').reduce((a, s, i) => a + (txt.charCodeAt(i) * (i + 1)), 0x12345678).toString();
 
   form.submit((e) => {
     e.stopPropagation();
@@ -118,16 +45,16 @@ $(document).ready(() => {
 
     // https://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
     const serialized = form.serializeArray();
-    const text = getText(serialized[0].value);
-    const color = toHex(serialized[1].value);
-    const node_colors = getNodeColors(serialized[2].value);
+    const text = Util.getText(serialized[0].value);
+    const color = Util.toHex(serialized[1].value);
+    const node_colors = Util.getNodeColors(serialized[2].value);
     const split = {
-      words: getSplitText(serialized[3].value),
-      color: toHex(serialized[4].value)
+      words: Util.getSplitText(serialized[3].value),
+      color: Util.toHex(serialized[4].value)
     };
-    const simple_pre_parsed = getSimpleParse(text);
-    const simple_path = getSimplePathStr(simple_pre_parsed);
-    const split_pre_parsed = split.words ? getSplitParse(text, split, color) : null;
+    const simple_pre_parsed = Util.getSimpleParse(text);
+    const simple_path = Util.getSimplePathStr(simple_pre_parsed);
+    const split_pre_parsed = split.words ? Util.getSplitParse(text, split, color) : null;
 
     // console.log(simple_path)
     console.log(split_pre_parsed)
@@ -156,8 +83,7 @@ $(document).ready(() => {
     }
 
     start_load();
-    essential_opts.checksum = checksum(JSON.stringify(essential_opts));
-    console.log(essential_opts.checksum)
+    essential_opts.checksum = Util.checksum(Util.reduceObj(essential_opts));
     console.log(essential_opts)
 
     $.ajax({
@@ -180,8 +106,15 @@ $(document).ready(() => {
           end_load()
         })
         .catch(err => {
-          console.log('err', err);
-          results.html(error(err.statusText));
+          if (err.statusText === 'error') {
+            const expected_url = `https://s3.amazonaws.com/word-art-svgs/${essential_opts.checksum}.svg`
+            // TODO get that working
+            // LongLoad.wait(expected_url).then(res=>console.log(res))
+            results.html(long_load_screen(expected_url));
+          } else {
+            console.log('Error', err);
+            results.html(error(err.statusText));
+          }
           end_load()
         })
 
